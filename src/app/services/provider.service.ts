a612cded61999150c5f6d1bd98b6db94a079a633
César Arana <carana@espol.edu.ec>
2024-08-14 23:42:14 -0500

    Eliminando comentarios

--- src/app/services/provider.service.ts
+++ src/app/services/provider.service.ts
@@ -1,29 +1,3 @@
-// import { Injectable } from '@angular/core';
-
-//  /* 1. Importe el módulo del HttpClient */
-//  import { HttpClient } from '@angular/common/http';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ProviderService {
-//      /* 2.Atributo URL */
-//      private URL: string = 'https://<NOMBRE_DEL_PROYECTO>.firebaseio.com/collection.json';
-
-//      /* 3. Inyección de dependencia del HttpClient */
-//      constructor(private http:HttpClient) { }
-
-//      /* 4. Método con la petición HTTP */
-//      getResponse() {
-//          return this.http.get(this.URL);
-//      }
-
-//      /* 5. Método con la petición HTTP */ 
-//      postResponse(data: any) {
-//          return this.http.post(this.URL, data);
-//      }
-// }
-// src/app/services/provider.service.ts
 import { Injectable } from '@angular/core';
 import { Preferences } from '@capacitor/preferences';
 import { Activity } from '../interfaces/activity';
